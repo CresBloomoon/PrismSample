4a0164a0d8f45bbee61000e54001d0d222e6784b
マサキ カタオカ <metroidm2421@gmail.com>
2022-08-04 02:49:12 +0900

    ラベルとボタンをバインドしました

--- PSamples/ViewModels/MainWindowViewModel.cs
+++ PSamples/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,11 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace PSamples.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "PrismSample";
         public string Title
         {
             get { return _title; }
@@ -13,7 +14,22 @@ namespace PSamples.ViewModels
 
         public MainWindowViewModel()
         {
+            SystemDateUpdateButton = new DelegateCommand(
+                SystemDateUpdateButtonExecute);
+        }
 
+        private string _systemDateLabel = System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        public string SystemDateLabel
+        {
+            get { return _systemDateLabel; }
+            set { SetProperty(ref _systemDateLabel, value); }
+        }
+
+        public DelegateCommand SystemDateUpdateButton { get; }
+        private void SystemDateUpdateButtonExecute()
+        {
+            SystemDateLabel =
+                System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
         }
     }
 }
